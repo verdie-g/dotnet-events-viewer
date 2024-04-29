59f1526c90a2ae336c41ee27aa42d7ae4d06011c
verdie-g <gregoire.verdier@gmail.com>
2024-04-29 16:01:57 -0400

    Move event name out of the filters (fix #21)

--- DotnetEventViewer/Querying/Query.cs
+++ DotnetEventViewer/Querying/Query.cs
@@ -1,6 +1,11 @@
+using DotnetEventViewer.CallTree.Decorators;
+
 namespace DotnetEventViewer.Querying;
 
 internal class Query
 {
-    public List<Filter> Filters { get; set; } = [];
+    public string[] EventNames { get; set; } = [];
+    public Filter[] Filters { get; set; } = [];
+    public IEnumerable<Field>? SelectedColumnFields { get; set; }
+    public ICallTreeNodeDecorator? SelectedDecorator { get; set; }
 }
