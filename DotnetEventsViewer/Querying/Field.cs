5500d31659058447304d5ebc9224d1dcf305875c
g.verdier <g.verdier@criteo.com>
2024-10-01 18:13:46 -0400

    Fix filters when two fields have the same name

--- DotnetEventsViewer/Querying/Field.cs
+++ DotnetEventsViewer/Querying/Field.cs
@@ -2,7 +2,7 @@ using EventPipe;
 
 namespace DotnetEventsViewer.Querying;
 
-public class Field(string name, TypeCode type, Func<Event, object?> selector, EventMetadata? associatedEventMetadata = null)
+public class Field(string name, TypeCode type, Func<Event, object?> selector, EventMetadata[]? associatedEventMetadata = null)
 {
     public static readonly Field CaptureThreadIdField = new(nameof(Event.CaptureThreadId), TypeCode.Int64, e => e.CaptureThreadId);
     public static readonly Field ThreadIdField = new(nameof(Event.ThreadId), TypeCode.Int64, e => e.ThreadId);
@@ -23,17 +23,21 @@ public class Field(string name, TypeCode type, Func<Event, object?> selector, Ev
         EventNameField,
     ];
 
-    public static Field FromPayloadFieldDefinition(EventFieldDefinition fieldDefinition, EventMetadata eventMetadata)
+    public static Field FromNameAndType(string name, TypeCode typeCode, EventMetadata[] eventMetadata)
     {
         return new Field(
-            fieldDefinition.Name,
-            fieldDefinition.TypeCode,
-            e => e.Payload.GetValueOrDefault(fieldDefinition.Name),
+            name,
+            typeCode,
+            e => e.Payload.GetValueOrDefault(name),
             eventMetadata);
     }
 
     public string Name { get; } = name;
     public TypeCode Type { get; } = type;
     public Func<Event, object?> Selector { get; } = selector;
-    public EventMetadata? AssociatedEventMetadata { get; } = associatedEventMetadata;
+
+    /// <summary>
+    /// The event the field is associated to. Can contain several events if the same name is used for different events.
+    /// </summary>
+    public EventMetadata[]? AssociatedEventMetadata { get; } = associatedEventMetadata;
 }
